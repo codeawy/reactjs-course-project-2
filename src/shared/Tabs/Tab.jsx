359debc8546f025388384167f2802039e815058b
codeawy <codeawy@gmail.com>
2023-02-06 21:04:46 +0200

    show tab content

--- src/shared/Tabs/Tab.jsx
+++ src/shared/Tabs/Tab.jsx
@@ -1,6 +1,9 @@
-const Tab = ({ children }) => {
+const Tab = ({ children, onClick }) => {
   return (
-    <div className="mx-3 bg-indigo-600 hover:bg-indigo-700 p-2 cursor-pointer duration-300 rounded-md text-sm">
+    <div
+      className="mx-3 bg-indigo-600 hover:bg-indigo-700 p-2 cursor-pointer duration-300 rounded-md text-sm"
+      onClick={onClick}
+    >
       {children}
     </div>
   );
--- src/shared/Tabs/Tabs.jsx
+++ src/shared/Tabs/Tabs.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { TABS_LIST } from "../../lists";
 import Tab from "./Tab";
 
+// ** A => String ❤
+
 const Tabs = () => {
+  const [activeTab, setActiveTab] = useState(TABS_LIST[0].name);
+  const [tempIdx, setTempIdx] = useState(0);
+
   return (
-    <div className="flex items-center justify-center my-10">
-      {TABS_LIST.map(tab => (
-        <Tab>{tab.title}</Tab>
-      ))}
+    <div>
+      <div className="flex items-center justify-center my-10">
+        {TABS_LIST.map((tab, idx) => (
+          <Tab key={tab.id} onClick={() => setTempIdx(idx)}>
+            {tab.title}
+          </Tab>
+        ))}
+      </div>
+      {TABS_LIST[tempIdx].tabPanel}
     </div>
   );
 };
